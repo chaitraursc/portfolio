31434ba064a81f2e68387278b4f02ec23240be01
citra <chaitraursc.de@gmail.com>
2026-03-12 17:28:10 +0100

    Add Tailwind, components, pages and styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  site: "https://chaitraursc.github.io",
+  base: "/portfolio", // omit this if deploying to root domain
+});
---
+++ src/content/reflections/on-switching-roles.md
@@ -0,0 +1,7 @@
+---
+title: "The way to PRINCE2 certification"
+date: "2026-03-12"
+description: "New ways of learning something in the era of AI"
+---
+
+## under construction
